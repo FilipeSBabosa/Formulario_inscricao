a0cd20c062c717327b0bd9ddc3f62e53be21e555
Lipe792 <144185319+FilipeSBabosa@users.noreply.github.com>
2024-04-27 00:05:08 -0300

    novo cor do botao

--- script.js
+++ script.js
@@ -2,6 +2,3 @@ document.getElementById("submit").addEventListener("submit", function(event) {
         event.preventDefault(); // Evita que o formulário seja enviado normalmente
         window.location.href = "inscrito.html"; // Redireciona para a nova página HTML
     });
-
-
-    
